9375ce1524d4b64d97610ccc27a045c4af9f0c91
Dick Childress <richard.childress@stratusgrid.com>
2022-11-09 13:31:13 -0500

    Replaced CloudFormation Stack resource with new Cost Explorer anomaly resources. Updated provider version constraint to >= 4.20.0, the version in which the new resources were added.

--- main.tf
+++ main.tf
@@ -1,29 +1,20 @@
-resource "aws_cloudformation_stack" "anomaly_detection" {
-  name          = var.name
-  tags          = local.tags
-  template_body = <<-STACK
-    Resources:
-      AnomalyMonitor${var.cost_threshold}monies:
-        Type: AWS::CE::AnomalyMonitor
-        Properties:
-          MonitorName: 'AnomalyDetected_is_greater_than_${var.cost_threshold}_monies'
-          MonitorType: 'DIMENSIONAL'
-          MonitorDimension: 'SERVICE'
+resource "aws_ce_anomaly_monitor" "this" {
+  name              = "AnomalyDetected_is_greater_than_${var.cost_threshold}_monies"
+  monitor_type      = "DIMENSIONAL"
+  monitor_dimension = "SERVICE"
 
-      AnomalySubscription:
-        Type: AWS::CE::AnomalySubscription
-        Properties:
-          SubscriptionName: AnomalyDetected_is_greater_than_${var.cost_threshold}_monies
-          Threshold: ${var.cost_threshold}
-          Frequency: DAILY
-          MonitorArnList:
-            - !Ref AnomalyMonitor${var.cost_threshold}monies
-          Subscribers:
-            [
-              {
-                "Type": "EMAIL",
-                "Address": ${var.subscription_email}
-              }
-            ]
-  STACK
+  tags = local.tags
+}
+
+resource "aws_ce_anomaly_subscription" "this" {
+  name             = "AnomalyDetected_is_greater_than_${var.cost_threshold}_monies"
+  monitor_arn_list = [aws_ce_anomaly_monitor.this.arn]
+  frequency        = "DAILY"
+  threshold        = var.cost_threshold
+  subscriber {
+    address = var.subscription_email
+    type    = "EMAIL"
+  }
+
+  tags = local.tags
 }
